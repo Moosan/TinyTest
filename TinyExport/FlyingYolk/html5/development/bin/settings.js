ddec002443dce8e7140ce9beb993d93ea2d2c767
Moosan <washer2016@outlook.jp>
2018-12-26 19:45:53 +0900

    add project

---
+++ TinyExport/FlyingYolk/html5/development/bin/settings.js
@@ -0,0 +1,18 @@
+﻿/**
+ * TINY GENERATED CODE, DO NOT EDIT BY HAND
+ * @project FlyingYolk
+ */
+
+var Module = {TOTAL_MEMORY: 16777216};
+
+Module.WSServerURL = "ws://10.32.222.155:17700/";
+Module.ProfilerServerURL = "ws://10.32.222.155:54997/";
+
+/*
+ * Workaround for issue UTINY-1091
+ */
+var ut = ut || {}
+ut.Shared = ut.Shared || {}
+ut.HTML = ut.HTML || {}
+ut.Rendering = ut.Rendering || {}
+ut.Profiler = ut.Profiler || {}
---
+++ TinyExport/GalaxyRaiders/html5/development/bin/settings.js
@@ -0,0 +1,17 @@
+﻿/**
+ * TINY GENERATED CODE, DO NOT EDIT BY HAND
+ * @project GalaxyRaiders
+ */
+
+var Module = {TOTAL_MEMORY: 16777216};
+
+Module.WSServerURL = "ws://10.32.222.155:17700/";
+Module.ProfilerServerURL = "ws://10.32.222.155:54997/";
+
+/*
+ * Workaround for issue UTINY-1091
+ */
+var ut = ut || {}
+ut.Shared = ut.Shared || {}
+ut.HTML = ut.HTML || {}
+ut.Rendering = ut.Rendering || {}
